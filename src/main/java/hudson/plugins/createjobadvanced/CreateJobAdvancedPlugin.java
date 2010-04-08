274b899c719b7f4e543f987e0fe31ea96edc0578
bertrandgressier <bertrandgressier@dev.java.net>
2010-04-08 15:20:49 +0000

    * Create an interface in hudson configuration     * activate/desactivate automatically set owner rights     * activate/desactivate replace space with "-" in the job name     * define default log rotator value

---
+++ src/main/java/hudson/plugins/createjobadvanced/CreateJobAdvancedPlugin.java
@@ -0,0 +1,123 @@
+package hudson.plugins.createjobadvanced;
+
+import hudson.Plugin;
+import hudson.Util;
+import hudson.model.Descriptor.FormException;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * @plugin
+ * @author Bertrand Gressier
+ * 
+ */
+
+public class CreateJobAdvancedPlugin extends Plugin {
+
+    static Logger log = Logger.getLogger(CreateJobAdvancedPlugin.class.getName());
+
+    private boolean autoOwnerRights;
+    private boolean replaceSpace;
+
+    private boolean activeLogRotator;
+    private int daysToKeep=-1;
+    private int numToKeep=-1;
+    private int artifactDaysToKeep=-1;
+    private int artifactNumToKeep=-1;
+
+    public CreateJobAdvancedPlugin() {
+    }
+
+    @Override
+    public void start() throws Exception {
+	// TODO Auto-generated method stub
+	super.start();
+	log.info("Create job advanced plugin started ...");
+	load();
+    }
+
+    @Override
+    public void configure(StaplerRequest req, JSONObject formData) throws IOException, ServletException, FormException {
+
+	// formData.optBoolean("autoOwnerRights",autoOwnerRights);
+
+	if (req.getParameter("cja.security") == null || req.getParameter("cja.security") == "false") {
+	    autoOwnerRights = false;
+	} else {
+	    autoOwnerRights = true;
+	}
+
+	if (req.getParameter("cja.jobspacesinname") == null || req.getParameter("cja.jobspacesinname") == "false") {
+	    replaceSpace = false;
+	} else {
+	    replaceSpace = true;
+	}
+
+	if (req.getParameter("cja.activeLogRotator") == null || req.getParameter("cja.activeLogRotator") == "false") {
+	    activeLogRotator = false;
+	} else {
+	    activeLogRotator = true;
+	}
+
+	if (activeLogRotator) {
+
+	    try {
+		daysToKeep = Integer.valueOf(Util.fixNull(req.getParameter("cja.daysToKeep")));
+	    } catch (Exception e) {
+		daysToKeep = -1;
+	    }
+	    try {
+		numToKeep = Integer.valueOf(Util.fixNull(req.getParameter("cja.numToKeep")));
+	    } catch (Exception e) {
+		numToKeep = -1;
+	    }
+	    try {
+		artifactDaysToKeep = Integer.valueOf(Util.fixNull(req.getParameter("cja.artifactDaysToKeep")));
+	    } catch (Exception e) {
+		artifactDaysToKeep = -1;
+	    }
+	    try {
+		artifactNumToKeep = Integer.valueOf(Util.fixNull(req.getParameter("cja.artifactNumToKeep")));
+	    } catch (Exception e) {
+		artifactNumToKeep = -1;
+	    }
+	}
+
+	save();
+    }
+
+    public boolean isAutoOwnerRights() {
+	return autoOwnerRights;
+    }
+
+    public boolean isReplaceSpace() {
+	return replaceSpace;
+    }
+
+    public boolean isActiveLogRotator() {
+	return activeLogRotator;
+    }
+
+    public int getDaysToKeep() {
+	return daysToKeep;
+    }
+
+    public int getNumToKeep() {
+	return numToKeep;
+    }
+
+    public int getArtifactDaysToKeep() {
+	return artifactDaysToKeep;
+    }
+
+    public int getArtifactNumToKeep() {
+	return artifactNumToKeep;
+    }
+}
---
+++ src/main/java/hudson/plugins/createjobadvanced/ItemListenerImpl.java
@@ -0,0 +1,150 @@
+package hudson.plugins.createjobadvanced;
+
+import hudson.Extension;
+import hudson.model.Hudson;
+import hudson.model.Item;
+import hudson.model.Job;
+import hudson.model.listeners.ItemListener;
+import hudson.security.AuthorizationMatrixProperty;
+import hudson.security.Permission;
+import hudson.security.SecurityMode;
+import hudson.tasks.LogRotator;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+@Extension
+public class ItemListenerImpl extends ItemListener {
+
+    static Logger log = Logger.getLogger(CreateJobAdvancedPlugin.class.getName());
+
+    @DataBoundConstructor
+    public ItemListenerImpl() {
+	//log.info("ItemListenerImpl started");
+    }
+
+    
+    @Override
+    public void onRenamed(Item item, String oldName, String newName) {
+       log.info("renamed "+oldName+" to "+newName);
+       
+       if (!(item instanceof Job))
+	    return;
+       final Job job = (Job) item;
+	
+       CreateJobAdvancedPlugin cja = Hudson.getInstance().getPlugin(CreateJobAdvancedPlugin.class);
+       if (cja.isReplaceSpace()) {
+	   renameJob(job);
+	}
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreated(Item item) {
+
+	if (!(item instanceof Job))
+	    return;
+	final Job job = (Job) item;
+
+	CreateJobAdvancedPlugin cja = Hudson.getInstance().getPlugin(CreateJobAdvancedPlugin.class);
+	
+	if (cja.isReplaceSpace()) {
+	   renameJob(job);
+	}
+	
+	if (cja.isAutoOwnerRights()){
+	    securityAutoOwner(job);
+	}
+	
+	if (cja.isActiveLogRotator()){
+	    activateLogRotator(job,cja);
+	}
+    }
+    
+    private void activateLogRotator(final Job job, final CreateJobAdvancedPlugin cja) {
+
+	//if template, it's possible that log rotator is already defined
+	if (job.getLogRotator() != null){
+	    return ;
+	}
+	
+	LogRotator logrotator = new LogRotator(cja.getDaysToKeep(), 
+		cja.getNumToKeep(), 
+		cja.getArtifactDaysToKeep(), 
+		cja.getArtifactNumToKeep());
+	
+	job.setLogRotator(logrotator);
+    }
+
+
+    private void renameJob(final Job job){
+	 if (job.getName().indexOf(" ") != -1) {
+		try {
+		    job.renameTo(job.getName().replaceAll(" ", "-"));
+		} catch (IOException e) {
+		    log.log(Level.SEVERE, "error during rename", e);
+		}
+	    }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void securityAutoOwner(final Job job){
+	// hudson must activate security mode for using
+	if (Hudson.getInstance().getSecurity().equals(SecurityMode.UNSECURED))
+	    return;
+
+	Map<Permission, Set<String>> permissions = null;
+
+	// if you create the job with template, need to get informations
+	AuthorizationMatrixProperty auth = (AuthorizationMatrixProperty) job
+		.getProperty(AuthorizationMatrixProperty.class);
+	if (auth != null) {
+	    permissions = new HashMap<Permission, Set<String>>(auth.getGrantedPermissions());
+	    try {
+		job.removeProperty(AuthorizationMatrixProperty.class);
+	    } catch (IOException e) {
+		log.log(Level.SEVERE, "problem to remove granted permissions (template or copy job)", e);
+	    }
+	} else {
+	    permissions = new HashMap<Permission, Set<String>>();
+	}
+
+	String sid = Hudson.getAuthentication().getName();
+
+	configurePermission(permissions, Item.CONFIGURE, sid);
+	configurePermission(permissions, Item.READ, sid);
+	configurePermission(permissions, Item.BUILD, sid);
+	configurePermission(permissions, Item.WORKSPACE, sid);
+	configurePermission(permissions, Item.DELETE, sid);
+
+	try {
+	    AuthorizationMatrixProperty authProperty = new AuthorizationMatrixProperty(permissions);
+	    job.addProperty(authProperty);
+	    log.info("Create Job " + job.getDisplayName() + " with right on " + sid);
+	} catch (IOException e) {
+	    log.log(Level.SEVERE, "problem to add granted permissions", e);
+	}
+    }
+    
+
+    private void configurePermission(Map<Permission, Set<String>> permissions, Permission permission, String sid) {
+
+	Set<String> sidPermission = permissions.get(permission);
+	if (sidPermission == null) {
+	    Set<String> sidSet = new HashSet<String>();
+	    sidSet.add(sid);
+	    permissions.put(permission, sidSet);
+	} else {
+	    if (!sidPermission.contains(sid)) {
+		sidPermission.add(sid);
+	    }
+	}
+    }
+}
