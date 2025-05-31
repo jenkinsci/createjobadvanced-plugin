357e20b70fa8f4a7e34f2fe0df3273a37f0266a3
Microscotch <github.com@microscotch.net>
2025-05-31 02:19:43 +0000

    Split tests per project kind

--- src/test/java/hudson/plugins/createjobadvanced/CreateJobAdvancedPluginTest.java
+++ src/test/java/hudson/plugins/createjobadvanced/CreateJobAdvancedPluginTest.java
@@ -2,15 +2,18 @@ package hudson.plugins.createjobadvanced;
 
 import com.cloudbees.hudson.plugins.folder.Folder;
 import hudson.maven.MavenModuleSet;
+import hudson.maven.reporters.MavenMailer;
+import hudson.model.AbstractItem;
+import hudson.model.FreeStyleProject;
 import hudson.model.Item;
-import hudson.model.Job;
+import hudson.model.TopLevelItem;
 import hudson.model.User;
 import hudson.security.ACL;
 import hudson.security.ACLContext;
-import hudson.security.AuthorizationMatrixProperty;
 import hudson.security.HudsonPrivateSecurityRealm;
 import hudson.security.Permission;
 import hudson.security.ProjectMatrixAuthorizationStrategy;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Objects;
 import jenkins.branch.OrganizationFolder;
@@ -29,9 +32,17 @@ public class CreateJobAdvancedPluginTest {
     @Rule
     public JenkinsRule r = new JenkinsRule();
 
+    private void initUser(String username) throws IOException {
+        // Initialize a user with the given username.
+        HudsonPrivateSecurityRealm realm = (HudsonPrivateSecurityRealm) r.jenkins.getSecurityRealm();
+        if (realm != null) {
+            realm.createAccount(username, username);
+        }
+    }
+
     @Test
-    @LocalData
-    public void loadFullyConfiguredCreateJobAdvanced() throws Exception {
+    @LocalData("createJobAdvancedFullConfig")
+    public void fullConfigTest() throws Exception {
         // This test checks if the Create Job Advanced plugin can load a fully configured job from local data.
         // The actual implementation of this test will depend on the specific configurations you have set up in your
         // local data.
@@ -53,98 +64,83 @@ public class CreateJobAdvancedPluginTest {
         Assert.assertTrue(cja.isMvnPerModuleEmail());
         Assert.assertTrue(cja.isActiveLogRotator());
         Assert.assertTrue(cja.isActiveDynamicPermissions());
+    }
 
-        Job<?, ?> job;
+    @Test
+    @LocalData("createJobAdvancedFullConfig")
+    public void freestyleTest() throws Exception {
+        initUser("alice");
+        FreeStyleProject project = createProject(FreeStyleProject.class, "Free Style Project", "alice");
+        Assert.assertEquals(project.getName().replaceAll(" ", "-"), project.getName());
+        testOwnerRights(project, "alice");
+    }
+
+    @Test
+    @LocalData("createJobAdvancedFullConfig")
+    public void mavenTest() throws Exception {
+        initUser("alice");
+        MavenModuleSet project = createProject(MavenModuleSet.class, "Maven Module Set", "alice");
+        Assert.assertEquals(project.getName().replaceAll(" ", "-"), project.getName());
+        testOwnerRights(project, "alice");
+        Assert.assertTrue(project.isArchivingDisabled());
+        MavenMailer m = project.getReporters().get(MavenMailer.class);
+        Assert.assertNotNull(m);
+        Assert.assertTrue(m.perModuleEmail);
+    }
+
+    @Test
+    @LocalData("createJobAdvancedFullConfig")
+    public void folderTest() throws Exception {
+        initUser("alice");
+        Folder project = createProject(Folder.class, "User Folder", "alice");
+        Assert.assertEquals(project.getName().replaceAll(" ", "-"), project.getName());
+        testOwnerRights(project, "alice");
+    }
+
+    @Test
+    @LocalData("createJobAdvancedFullConfig")
+    public void pipelineTest() throws Exception {
+        initUser("alice");
+        WorkflowJob project = createProject(WorkflowJob.class, "Workflow Job", "alice");
+        Assert.assertEquals(project.getName().replaceAll(" ", "-"), project.getName());
+        testOwnerRights(project, "alice");
+    }
+
+    @Test
+    @LocalData("createJobAdvancedFullConfig")
+    public void multibranchTest() throws Exception {
+        initUser("alice");
+        WorkflowMultiBranchProject project =
+                createProject(WorkflowMultiBranchProject.class, "Workflow Multi Branch Project", "alice");
+        Assert.assertEquals(project.getName().replaceAll(" ", "-"), project.getName());
+        testOwnerRights(project, "alice");
+    }
+
+    @Test
+    @LocalData("createJobAdvancedFullConfig")
+    public void organizationFolderTest() throws Exception {
+        initUser("alice");
+        OrganizationFolder project = createProject(OrganizationFolder.class, "Organization Folder", "alice");
+        Assert.assertEquals(project.getName().replaceAll(" ", "-"), project.getName());
+        testOwnerRights(project, "alice");
+    }
+
+    private <T extends TopLevelItem> T createProject(Class<T> jobClass, String name, String creator) throws Exception {
+        // This method creates a project of the specified type with the given name.
+        T project;
         try (ACLContext ignored = ACL.as(User.get("alice", false, Collections.emptyMap()))) {
-            job = r.createFreeStyleProject("test freestyle");
+            project = r.createProject(jobClass, name);
         }
-        Assert.assertNotNull(job.getProperty(AuthorizationMatrixProperty.class));
-        Assert.assertEquals("test-freestyle", job.getName());
+        return project;
+    }
 
+    private void testOwnerRights(AbstractItem item, String owner) throws Exception {
+        // This method checks if the owner rights are correctly set for the given job.
         for (Permission permission :
                 new Permission[] {Item.CONFIGURE, Item.BUILD, Item.READ, Item.DELETE, Item.WORKSPACE}) {
-            Assert.assertTrue(job.getACL()
+            Assert.assertTrue(item.getACL()
                     .hasPermission2(
-                            Objects.requireNonNull(User.get("alice", false, Collections.emptyMap()))
-                                    .impersonate2(),
-                            permission));
-        }
-
-        // Maven job
-        try (ACLContext ignored = ACL.as(User.get("alice", false, Collections.emptyMap()))) {
-            job = r.createProject(MavenModuleSet.class, "test maven");
-        }
-        Assert.assertNotNull(job.getProperty(AuthorizationMatrixProperty.class));
-        Assert.assertEquals("test-maven", job.getName());
-
-        for (Permission permission :
-                new Permission[] {Item.CONFIGURE, Item.BUILD, Item.READ, Item.DELETE, Item.WORKSPACE}) {
-            Assert.assertTrue(job.getACL()
-                    .hasPermission2(
-                            Objects.requireNonNull(User.get("alice", false, Collections.emptyMap()))
-                                    .impersonate2(),
-                            permission));
-        }
-
-        // Folder job
-        Folder folder;
-        try (ACLContext ignored = ACL.as(User.get("alice", false, Collections.emptyMap()))) {
-            folder = r.createProject(Folder.class, "test-folder");
-        }
-        Assert.assertEquals("test-folder", folder.getName());
-
-        for (Permission permission :
-                new Permission[] {Item.CONFIGURE, Item.BUILD, Item.READ, Item.DELETE, Item.WORKSPACE}) {
-            Assert.assertTrue(folder.getACL()
-                    .hasPermission2(
-                            Objects.requireNonNull(User.get("alice", false, Collections.emptyMap()))
-                                    .impersonate2(),
-                            permission));
-        }
-
-        // Pipeline job
-        try (ACLContext ignored = ACL.as(User.get("alice", false, Collections.emptyMap()))) {
-            job = r.createProject(WorkflowJob.class, "test pipeline");
-        }
-        Assert.assertNotNull(job.getProperty(AuthorizationMatrixProperty.class));
-        Assert.assertEquals("test-pipeline", job.getName());
-
-        for (Permission permission :
-                new Permission[] {Item.CONFIGURE, Item.BUILD, Item.READ, Item.DELETE, Item.WORKSPACE}) {
-            Assert.assertTrue(job.getACL()
-                    .hasPermission2(
-                            Objects.requireNonNull(User.get("alice", false, Collections.emptyMap()))
-                                    .impersonate2(),
-                            permission));
-        }
-
-        WorkflowMultiBranchProject mbp;
-        try (ACLContext ignored = ACL.as(User.get("alice", false, Collections.emptyMap()))) {
-            mbp = r.createProject(WorkflowMultiBranchProject.class, "test multibranch");
-        }
-        // Assert.assertNotNull(mbp.getProperty(AuthorizationMatrixProperty.class));
-        Assert.assertEquals("test-multibranch", mbp.getName());
-
-        for (Permission permission :
-                new Permission[] {Item.CONFIGURE, Item.BUILD, Item.READ, Item.DELETE, Item.WORKSPACE}) {
-            Assert.assertTrue(mbp.getACL()
-                    .hasPermission2(
-                            Objects.requireNonNull(User.get("alice", false, Collections.emptyMap()))
-                                    .impersonate2(),
-                            permission));
-        }
-
-        OrganizationFolder orgFolder;
-        try (ACLContext ignored = ACL.as(User.get("alice", false, Collections.emptyMap()))) {
-            orgFolder = r.createProject(OrganizationFolder.class, "test org folder");
-        }
-        Assert.assertEquals("test-org-folder", orgFolder.getName());
-        for (Permission permission :
-                new Permission[] {Item.CONFIGURE, Item.BUILD, Item.READ, Item.DELETE, Item.WORKSPACE}) {
-            Assert.assertTrue(orgFolder
-                    .getACL()
-                    .hasPermission2(
-                            Objects.requireNonNull(User.get("alice", false, Collections.emptyMap()))
+                            Objects.requireNonNull(User.get(owner, false, Collections.emptyMap()))
                                     .impersonate2(),
                             permission));
         }
